1b75443f2cf056ee07a8c540fa5ea842f2af4d53
akramboy05 <akramboy05r@gmail.com>
2022-04-17 14:11:44 +0500

    ashjdjs

--- src/components/BannerBooks/BannerBooks.js
+++ src/components/BannerBooks/BannerBooks.js
@@ -64,14 +64,14 @@ const BannerBooks = () => {
                 </div>
                </Link>
         </div>
-        <div className="banner_text">
+        {/* <div className="banner_text">
             <h1>WHY INDEPENDENCE MATTERS</h1>
             <p>Think about the last good book you read. Did it make you feel more connected to others? Maybe it served as a welcome escape. Maybe it helped you rediscover the beauty in life. Did it surprise you?</p>
 
             <p>As an independent bookstore, we strive to offer the same variety and richness of experience as the books on our shelves. And because the only people we’re beholden to are our customers and ourselves, we can focus on what really matters — promoting diverse perspectives, upholding the free exchange of ideas, championing the enduring power of books, and bolstering the great community of readers and authors we’re lucky to be a part of.</p>
             <p>
             Thank you for supporting these lofty goals. Your choice sustains a family business, and allows us to follow our passion for getting the right books into the right hands, 365 days a year.</p>
-        </div>
+        </div> */}
     </div>
   )
 }
